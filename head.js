23026101b580b8c7f85384770a14f210d73432d8
meteora277 <justin.s.diaz@gmail.com>
2021-11-22 14:59:24 -0500

    created head function, a function that returns the head of an array (array[0])

---
+++ head.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+
+    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+
+  } else {
+
+    return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+
+  }
+
+};
+
+const head = function(arr) {
+
+  if (Array.isArray(arr)) {
+
+    return arr[0];
+
+  } else {
+
+    return undefined;
+
+  }
+
+};
+console.log(assertEqual(head([5,6,7]), 5));
+console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
+console.log(assertEqual(head([]), 5));
+console.log(assertEqual(head(["owo", "Lighthouse", "Labs"]), "Hello"));
