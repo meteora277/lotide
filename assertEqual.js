0a7f1697502adf06bfabff318b62a25bc46c313e
meteora277 <justin.s.diaz@gmail.com>
2021-11-22 14:33:22 -0500

    created an alternative function to console.assert

---
+++ assertEqual.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+
+    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+
+  } else {
+
+    return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+
+  }
+
+};
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
+console.log(assertEqual("uwu", "uwu"));
+console.log(assertEqual(3,2));
