f008bcfe0a52dabd05e95e5362df900c2b7c8a33
meteora277 <justin.s.diaz@gmail.com>
2021-11-23 13:57:34 -0500

    created eqArray function, it will take in two arrays and loop through to see if items inside are the same

---
+++ eqArrays.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+
+    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+
+  } else {
+
+    return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+
+  }
+
+};
+
+
+const eqArrays = function(array1, array2) {
+
+  for (let i = 0; i < array1.length; i++) {
+
+    if (array1[i] !== array2[i]) {
+
+      return false;
+
+    }
+
+  }
+  return true;
+};
+
+console.log(assertEqual(eqArrays([1, 2, "3"], [1, 2, "3"]), true));
+console.log(assertEqual(eqArrays([],[]),true));
