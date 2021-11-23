0909c28995f4204a67f1ac7300c48fe5b8fc0b90
meteora277 <justin.s.diaz@gmail.com>
2021-11-23 15:08:06 -0500

    defined without function, which removes items from an array

---
+++ without.js
@@ -0,0 +1,15 @@
+const without = function(array, excludes) {
+  
+  let newArray = [...array];
+  
+  for (let exclude of excludes) {
+    
+    newArray = newArray.filter(item => item !== exclude);
+  }
+
+  return newArray;
+
+};
+
+console.log(without([1, 2, 3], [1]));
+console.log(without(["1", "2", "3"],[1, 2, "3"]));
