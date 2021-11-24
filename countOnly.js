606f972c0f7f3fed06a83f0bb1432c0ddb891d21
meteora277 <justin.s.diaz@gmail.com>
2021-11-23 21:38:04 -0500

    created the logic of countOnly

--- countOnly.js
+++ countOnly.js
@@ -12,8 +12,45 @@ const assertEqual = function(actual, expected) {
 
 };
 
-const countOnly = function() { 
+const countOnly = function(allItems, itemsToCount) {
 
-  
+  const results = {};
 
-}
+  for (const item of allItems) {
+
+    console.log(item);
+    if (itemsToCount[item]) {
+      if (results[item]) {
+
+        results[item] += 1;
+
+      } else {
+
+        results[item] = 1;
+
+      }
+    } 
+  }
+  console.log(results)
+  return results;
+
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, {"Jason" : true, "Karima": true, "Fang": true, "Agouhanna": false});
+
+console.log(assertEqual(result1["Jason"], 1));
+console.log(assertEqual(result1["Karima"], undefined));
+console.log(assertEqual(result1["Fang"], 2));
+console.log(assertEqual(result1["Agouhanna"], undefined));
