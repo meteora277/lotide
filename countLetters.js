31dd4bbfcb043261dc6fdaa85a1b05887f4c71fe
meteora277 <justin.s.diaz@gmail.com>
2021-11-23 21:52:45 -0500

    defined countLetters function

---
+++ countLetters.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+
+    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+
+  } else {
+
+    return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+
+  }
+
+};
+
+const countLetters = function(sentence) {
+
+  let noSpaces = sentence.split(" ").join("");
+  let charactersObject = {};
+
+  for (let letter of noSpaces) {
+
+    if (charactersObject[letter]) {
+      charactersObject[letter] += 1;
+    } else {
+      charactersObject[letter] = 1;
+    }
+  }
+  return charactersObject;
+};
+
+console.log(countLetters("lighthouse in the house"));
