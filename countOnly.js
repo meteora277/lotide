d3d5be2d0401b33fd5f805c1a9ae72f5e049530c
meteora277 <justin.s.diaz@gmail.com>
2021-11-23 21:08:05 -0500

    defined count only function

---
+++ countOnly.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+
+    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+
+  } else {
+
+    return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+
+  }
+
+};
+
+const countOnly = function() { 
+
+  
+
+}
