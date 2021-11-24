2ea608c4ecd828e09c8b10aa7b01dd709f72b4dd
meteora277 <justin.s.diaz@gmail.com>
2021-11-23 23:03:01 -0500

    added the logic for findKeysByValue

---
+++ findKeysByValue.js
@@ -0,0 +1,37 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+
+    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+
+  } else {
+
+    return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+
+  }
+
+};
+
+const findKeyByValue = function(object, value) {
+
+  for (let key in object) {
+
+    if (object[key] === value) {
+
+      return key;
+
+    }
+
+  }
+
+};
+
+const bestTVShowsByGenre = {
+  "sci_fi": "The Expanse",
+  "comedy": "Brooklyn Nine-Nine",
+  "drama":  "The Wire"
+};
+console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"),"drama"));
+console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined));
+console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"),"sci_fi"));
+console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "uwu"), undefined));
