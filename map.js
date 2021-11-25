327b5065d11e4c238bdaaa3ff0adee354c626b70
meteora277 <justin.s.diaz@gmail.com>
2021-11-24 21:22:10 -0500

    defined map function to a cut down version of Array.map

---
+++ map.js
@@ -0,0 +1,36 @@
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertEqualArrays = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  let results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+assertEqualArrays(
+  map(words, (word) => word[0]),["g", "c", "t", "m", "t"]
+);
+assertEqualArrays(
+  map(words, (word) => word + " uwu"),["ground uwu", "control uwu", "to uwu", "major uwu", "tom uwu"]
+);
+assertEqualArrays(
+  map(words, (word) => word.split("").map(letter => letter === "r" ? "ow" : letter).join("")),["gowound", "contowol", "to", "majoow", "tom"]
+);
