c7d6f484c464511cdfe7c193a7ad6488b990002f
meteora277 <justin.s.diaz@gmail.com>
2021-11-29 14:37:24 -0500

    imported chai and mocha, converted inbuilt assert to chai.assert

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,40 @@
 const head = require("../head");
-const assertEqual = require("../assertEqual");
-console.log(assertEqual(head([5,6,7]), 5));
-console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
-console.log(assertEqual(head([]), 5));
-console.log(assertEqual(head(["owo", "Lighthouse", "Labs"]), "Hello"));
+const { assert, expect } = require("chai");
+const { array } = require("yargs");
+
+describe("#head", () => {
+
+  it("returns 5 for [5, 6, 7]", () => {
+
+    assert.strictEqual(head([5, 6, 7]),5);
+
+  });
+
+});
+describe("#head", () => {
+
+  it("returns 5 for [5]", () => {
+
+    assert.strictEqual(head([5]),5);
+
+  });
+
+});
+describe("#head", () => {
+
+  it("returns Hello for head(['Hello', 'Lighthouse', 'Labs'])", () => {
+
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]),"Hello");
+
+  });
+
+});
+describe("#head", () => {
+
+  it("returns undefined if passed in []", () => {
+
+    expect(head([])).to.be.undefined;
+
+  });
+
+});
