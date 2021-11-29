38d7b1092942f36013d3c92d8288d4ad8d217882
meteora277 <justin.s.diaz@gmail.com>
2021-11-29 14:54:24 -0500

    created test cases with mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,35 @@
-const assertEqual = require("../assertEqual");
 const tail = require("../tail");
+const { assert, expect } = require('chai');
 
-const result = tail(["Hello", "Lighthouse", "Labs"])[0];
-console.log(assertEqual(result, "Lighthouse"));
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(assertEqual(words.length,3));
+describe("tail ()", () => {
+
+  it("should return ['Lighthouse', 'Labs'] when passed in ['Hello', 'Lighthouse', 'Labs']", () => {
+
+    const actual = tail(['Hello', 'Lighthouse', 'Labs']);
+    const expected = ['Lighthouse', 'Labs'];
+
+    assert.deepEqual(actual, expected);
+
+  });
+
+});
+
+describe("tail ()", () => {
+
+  it("should return ['Lighthouse', 5] when passed in ['Hello', 'Lighthouse', 5]", () => {
+
+    const actual = tail(['Hello', 'Lighthouse', 5]);
+    const expected = ['Lighthouse', 5];
+
+    assert.deepEqual(actual, expected);
+
+  });
+
+});
+
+
+
+// const result = tail(["Hello", "Lighthouse", "Labs"])[0];
+// console.log(assertEqual(result, "Lighthouse"));
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// console.log(assertEqual(words.length,3));
