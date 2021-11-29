df042a066856541a7a83a0cbe7af798d2a795718
meteora277 <justin.s.diaz@gmail.com>
2021-11-29 13:05:07 -0500

    removed commented out console.logs

--- head.js
+++ head.js
@@ -13,7 +13,3 @@ const head = function(arr) {
 };
 
 module.exports = head;
-// console.log(assertEqual(head([5,6,7]), 5));
-// console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
-// console.log(assertEqual(head([]), 5));
-// console.log(assertEqual(head(["owo", "Lighthouse", "Labs"]), "Hello"));
