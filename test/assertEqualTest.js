7430576101bfd4c06ddd35786265ffa5f21d95a5
meteora277 <justin.s.diaz@gmail.com>
2021-11-29 10:55:38 -0500

    created folder for test files

---
+++ test/assertEqualTest.js
@@ -0,0 +1,6 @@
+const assertEqual = require('../assertEqual');
+
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
+console.log(assertEqual("uwu", "uwu"));
+console.log(assertEqual(3,2));
