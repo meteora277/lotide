697c50e2aa4dc169c72b753c6fd1248de918edd2
meteora277 <justin.s.diaz@gmail.com>
2021-11-29 13:14:48 -0500

    moved assertions into the tailTest file

--- tail.js
+++ tail.js
@@ -1,16 +1,3 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-
-    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
-
-  } else {
-
-    return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
-
-  }
-
-};
 const tail = function(arr) {
 
   let newArr = arr.slice(1);
@@ -18,9 +5,5 @@ const tail = function(arr) {
   return newArr;
 
 };
+module.exports = tail;
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-console.log(assertEqual(result, ["Lighthouse", "Labs"]));
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log([words]);
-console.log(assertEqual(words.length,3));
