0828843bde7cb000b63250744f53ee4ece0519dc
meteora277 <justin.s.diaz@gmail.com>
2021-11-29 10:56:26 -0500

    moved test cases into its own folder

--- assertEqual.js
+++ assertEqual.js
@@ -11,7 +11,5 @@ const assertEqual = function(actual, expected) {
   }
 
 };
-console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
-console.log(assertEqual(1, 1));
-console.log(assertEqual("uwu", "uwu"));
-console.log(assertEqual(3,2));
+
+module.exports = assertEqual;
